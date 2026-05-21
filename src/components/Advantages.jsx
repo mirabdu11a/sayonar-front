950d6af80eab94114a643702f0c5b0004d0d17a5
jamatov <mirabdullajamatov@gmail.com>
2026-05-21 11:29:08 +0500

    front

--- src/components/Advantages.jsx
+++ src/components/Advantages.jsx
@@ -28,56 +28,6 @@ export default function Advantages() {
           <button>{t("more")}</button>
         </div>
         <div className="row">
-<<<<<<< HEAD
-          <div className="col-md-4 block">
-            <div className="card1">
-              <img src={ico1} alt="icon" />
-              <h3>{t("advantagesCardTitle1")}</h3>
-              <p>{t("advantagesCardInfo1")}</p>
-            </div>
-          </div>
-
-          <div className="col-md-4 block">
-            <div className="card1">
-              <img src={ico2} alt="icon" />
-              <h3>{t("advantagesCardTitle2")}</h3>
-              <p>{t("advantagesCardInfo2")}</p>
-            </div>
-          </div>
-
-          <div className="col-md-4 block">
-            <div className="card1">
-              <img src={ico3} alt="icon" />
-              <h3>{t("advantagesCardTitle3")}</h3>
-              <p>{t("advantagesCardInfo3")}</p>
-            </div>
-          </div>
-
-          <div className="col-md-4 block">
-            <div className="card1">
-              <img src={ico4} alt="icon" />
-              <h3>{t("advantagesCardTitle4")}</h3>
-              <p>{t("advantagesCardInfo4")}</p>
-            </div>
-          </div>
-
-          <div className="col-md-4 block">
-            <div className="card1">
-              <img src={ico5} alt="icon" />
-              <h3>{t("advantagesCardTitle5")}</h3>
-              <p>{t("advantagesCardInfo5")}</p>
-            </div>
-          </div>
-
-          <div className="col-md-4 block">
-            <div className="card1">
-              <img src={ico6} alt="icon" />
-              <h3>{t("advantagesCardTitle6")}</h3>
-              <p>{t("advantagesCardInfo6")}</p>
-            </div>
-          </div>
-
-=======
           {!loading && services.map((s, i) => (
             <div className="col-md-4 block" key={s.id}>
               <div className="card1">
@@ -87,7 +37,6 @@ export default function Advantages() {
               </div>
             </div>
           ))}
->>>>>>> 7f611e84a21731e766a2a1ffd20fe10ce511e37c
         </div>
       </div>
     </section>
