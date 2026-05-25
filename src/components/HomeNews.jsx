c29784437ce8039a38b7512c692c6ec0016ed0fd
jamatov <mirabdullajamatov@gmail.com>
2026-05-25 17:27:16 +0500

    news position changed

--- src/components/HomeNews.jsx
+++ src/components/HomeNews.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination } from 'swiper/modules';
 
@@ -12,74 +12,138 @@ import { fetchNews } from '../api/endpoints';
 import { useLocale } from '../utils/locale';
 import { mediaUrl } from '../api/client';
 import { useTranslation } from "react-i18next";
-import { NavLink } from 'react-router-dom';
 
 export default function HomeNews() {
   const { data, loading } = useApi(fetchNews);
   const p = useLocale();
-  const news = data || [];
   const { t } = useTranslation();
+
+  // Eng oxirgi yuklangan yangilik birinchi chiqishi uchun
+  const news = [...(data || [])].reverse();
 
   return (
     <section className='HomeNews'>
       <div className="container">
+
         <div className="section-tittle">
           <div className='first-block col-md-3'>
             <div className="disc"></div>
             <p>{t("homeNewsInfo")}</p>
           </div>
-          <div className='second-block'>
-            <h2 className='col-md-9'>{t("homeNewsTitle")}</h2>
-            <NavLink to="/news">
-              <button>{t("moreNews")}</button>
-            </NavLink>
 
+          <div className='second-block'>
+            <h2 className='col-md-9'>
+              {t("homeNewsTitle")}
+            </h2>
+
+            <NavLink to="/news">
+              <button>
+                {t("moreNews")}
+              </button>
+            </NavLink>
           </div>
         </div>
 
         <div className="news-slider-wrapper">
+
           {!loading && (
             <Swiper
               modules={[Navigation, Pagination]}
               spaceBetween={30}
               slidesPerView={3}
-              pagination={{ clickable: true, el: '.custom-pagination' }}
+              pagination={{
+                clickable: true,
+                el: '.custom-pagination',
+              }}
               navigation={{
                 nextEl: '.swiper-button-next-custom',
                 prevEl: '.swiper-button-prev-custom',
               }}
               breakpoints={{
-                320: { slidesPerView: 1 },
-                768: { slidesPerView: 2 },
-                1024: { slidesPerView: 3 },
+                320: {
+                  slidesPerView: 1,
+                },
+                768: {
+                  slidesPerView: 2,
+                },
+                1024: {
+                  slidesPerView: 3,
+                },
               }}
               className="news-swiper"
             >
+
               {news.map((item) => (
                 <SwiperSlide key={item.id}>
-                  <Link to={`/news/${item.id}`} className="news-card">
-                    <img src={mediaUrl(item.image)} alt={p(item, 'title')} />
-                    <h4>{p(item, 'title')}</h4>
-                    <h5>{p(item, 'tizer')}</h5>
+
+                  <Link
+                    to={`/news/${item.id}`}
+                    className="news-card"
+                  >
+
+                    <img
+                      src={mediaUrl(item.image)}
+                      alt={p(item, 'title')}
+                    />
+
+                    <h4>
+                      {p(item, 'title')}
+                    </h4>
+
+                    <h5>
+                      {p(item, 'tizer')}
+                    </h5>
+
                   </Link>
+
                 </SwiperSlide>
               ))}
+
             </Swiper>
           )}
 
           <div className="slider-controls">
+
             <div className="swiper-button-prev-custom">
-              <svg xmlns="http://www.w3.org/2000/svg" width="8" height="14" viewBox="0 0 8 14" fill="none">
-                <path d="M7 1L1 7L7 13" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="8"
+                height="14"
+                viewBox="0 0 8 14"
+                fill="none"
+              >
+                <path
+                  d="M7 1L1 7L7 13"
+                  stroke="black"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
               </svg>
             </div>
+
             <div className="custom-pagination"></div>
+
             <div className="swiper-button-next-custom">
-              <svg xmlns="http://www.w3.org/2000/svg" width="8" height="14" viewBox="0 0 8 14" fill="none">
-                <path d="M1 13L7 7L1 1" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="8"
+                height="14"
+                viewBox="0 0 8 14"
+                fill="none"
+              >
+                <path
+                  d="M1 13L7 7L1 1"
+                  stroke="black"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
               </svg>
             </div>
+
           </div>
+
         </div>
       </div>
     </section>
--- src/components/NewsBody.jsx
+++ src/components/NewsBody.jsx
@@ -9,28 +9,58 @@ import { useTranslation } from "react-i18next";
 export default function NewsBody() {
   const { data, loading } = useApi(fetchNews);
   const p = useLocale();
-  const news = data || [];
   const { t } = useTranslation();
+
+  // Eng oxirgi yuklangan yangilik birinchi chiqadi
+  const news = [...(data || [])].reverse();
 
   return (
     <section className='NewsBody'>
       <div className="container">
+
         <div className="row">
           <div className="col-md-6 title">
-            <h2>{t("newsBodyTitle")}</h2>
-            <p>{t("newsBodyInfo")}</p>
+            <h2>
+              {t("newsBodyTitle")}
+            </h2>
+
+            <p>
+              {t("newsBodyInfo")}
+            </p>
           </div>
         </div>
+
         <div className="row">
+
           {!loading && news.map((item) => (
-            <div className="col-md-4 block" key={item.id}>
-              <Link to={`/news/${item.id}`} className="news-card">
-                <img src={mediaUrl(item.image)} alt={p(item, 'title')} />
-                <h4>{p(item, 'title')}</h4>
-                <h5>{p(item, 'tizer')}</h5>
+            <div
+              className="col-md-4 block"
+              key={item.id}
+            >
+
+              <Link
+                to={`/news/${item.id}`}
+                className="news-card"
+              >
+
+                <img
+                  src={mediaUrl(item.image)}
+                  alt={p(item, 'title')}
+                />
+
+                <h4>
+                  {p(item, 'title')}
+                </h4>
+
+                <h5>
+                  {p(item, 'tizer')}
+                </h5>
+
               </Link>
+
             </div>
           ))}
+
         </div>
       </div>
     </section>
