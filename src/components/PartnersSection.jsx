7d01d7aedc14ae4a2ae03d4efd811dd1f8595602
jamatov <mirabdullajamatov@gmail.com>
2026-06-02 17:55:56 +0500

    add link to pafrtners section

--- src/components/PartnersSection.jsx
+++ src/components/PartnersSection.jsx
@@ -49,12 +49,31 @@ export default function PartnersSection() {
         </div>
 
         <div className="logo-body">
-          {!loading && partners.map((partner) => 
-            (partner.images || []).map((img) => (
-              <div key={img.id} className="logo-item">
-                <img src={mediaUrl(img.image)} alt={p(partner, 'title')} />
-              </div>
-            ))
+          {!loading && partners.map((partner) =>
+            (partner.images || []).map((img) => {
+              const content = (
+                <img
+                  src={mediaUrl(img.image)}
+                  alt={p(partner, 'title')}
+                />
+              );
+
+              return img.site_url ? (
+                <a
+                  key={img.id}
+                  className="logo-item"
+                  href={img.site_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {content}
+                </a>
+              ) : (
+                <div key={img.id} className="logo-item">
+                  {content}
+                </div>
+              );
+            })
           )}
         </div>
       </div>
